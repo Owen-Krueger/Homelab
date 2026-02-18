9fd221987af46832e8f7372d4a8ce59213bc262e
Owen Krueger <github@owenkrueger.dev>
2026-02-18 13:31:17 -0600

    Deploy matrix server

---
+++ Ansible/scripts/deploy_matrix.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-playbook ../playbooks/deploy_matrix.yaml -i ../inventory.yaml
---
+++ Ansible/scripts/update_caddy_config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-playbook ../playbooks/update_caddy_config.yaml -i ../inventory.yaml
